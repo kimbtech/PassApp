9b5038c0b384465a3f08bfbfb2e5f55c25a2700a
KIMB-technologies <info@kimb-technologies.eu>
2018-09-09 11:56:17 +0200

    Update hintsOverlay.js

--- sys/hintsOverlay.js
+++ sys/hintsOverlay.js
@@ -1,3 +1,9 @@
+/**
+ * PassApp by KIMB-technologies
+ * (c) 2018, released unter terms of MIT License
+ * https://github.com/kimbtech/PassApp
+ */
+
 function hintsOverlay(){
 	const openHintsButton = "button#openHints";
 	const closeHintsButton = "button#closeHints"
